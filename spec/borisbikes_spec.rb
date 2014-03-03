cfccf65679d09bfec348455a5ef1ccd85d09c5dc
Hannah <hannah_wight@hotmail.com>
2014-03-03 16:56:55 +0000

    Added lib and spec folders to repo

---
+++ spec/borisbikes_spec.rb
@@ -0,0 +1,13 @@
+require_relative "../lib/bike"
+
+describe Bike do
+	it "should not be broken after we create it" do
+		bike = Bike.new
+		expect(bike.broken?).to be_false
+	end
+	it "should be able to break" do 
+		bike = Bike.new
+		bike.break
+		expect(bike).to be_broken
+	end
+end
